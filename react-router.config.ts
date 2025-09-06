b7ad135e70993a73767c8c6d3b2e7ee0405def25
idrury <isaac@lightworksproductions.au>
2025-09-06 13:24:58 +0930

    Update tags

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
-  async prerender() {
-    return ["/", "/portfolio", "/contact"];
-    ;
-  },
+  // async prerender() {
+  //   return ["/", "/portfolio", "/contact"];
+  //   ;
+  // },
   ssr: false,
 } satisfies Config;
